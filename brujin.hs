a81022013ed943c63f32141d98ce0ea7543efe2d
smudgecat123 <jf14218@my.bristol.ac.uk>
2018-03-08 18:05:16 +0000

    wrote my ideas

--- brujin.hs
+++ brujin.hs
@@ -25,16 +25,21 @@ instance Show Brexpr where
 ex1 = App (Lam (Lam (App (App (Var 4) (Var 2)) (Lam (App (Var 1) (Var 3)))))) (Lam (App (Var 5) (Var 1)))
 
 --this increments all the "free" variables within an expression, the higher k is, the fewer variables are considered free
-inc :: Brexpr -> Int -> Index -> Brexpr
-inc     (Var i) n k = Var $ i + (if i < k then 0 else n)
-inc (App e1 e2) n k = App (inc e1 n k) (inc e2 n k)
-inc    (Lam e1) n k = Lam $ inc e1 n (k+1)
+shift :: Brexpr -> Int -> Index -> Brexpr
+shift     (Var i) n k = Var $ i + (if i < k then 0 else n)
+shift (App e1 e2) n k = App (shift e1 n k) (shift e2 n k)
+shift    (Lam e1) n k = Lam $ shift e1 n (k+1)
 
---this substitutes all variables referencing the same lam with an expression, e, and corrects all indecies to avoid capture
+{-
+This function substitutes all variables in expression, a, that reference the removed lam with an expression, e.
+It also corrects free variables of e to avoid them being captured.
+It also decrements the free variables of the a to correct them after the outer lam was removed (due to beta reduction).
+Because of this, its functionality is only correct in the context of it being called from "beta" function.
+-}
 sub :: Brexpr -> Index -> Brexpr -> Brexpr
 sub (Var i) k e
   |  i < k          = Var i
-  | i == k          = inc e (k-1) 1 --this had to be changed from "inc e k 0" to work correctly
+  | i == k          = shift e (k-1) 1 --this had to be changed from "inc e k 0" to correct two seperate problems
   |  i > k          = Var $ i - 1
 sub (App e1 e2) k e = App (sub e1 k e) (sub e2 k e)
 sub    (Lam e1) k e = Lam $ sub e1 (k+1) e
@@ -43,3 +48,11 @@ sub    (Lam e1) k e = Lam $ sub e1 (k+1) e
 beta :: Brexpr -> Brexpr
 beta (App (Lam e1) e) = sub e1 1 e --"1" represents all variables which were bound by the Lam in "Lam e1"
 beta e = e
+
+{-
+The first problem was that variables which were incremented were incremented 1 too many times.
+Changing k to k-1 fixes this.
+The second problem was that bound variables with an index of 1 were being incremented because of the
+conditional logic in the shift function.
+Changing 0 to 1 fixes this. As does changing "<" to "<=" in the shift function itself.
+-}
--- lcc/Main.hs
+++ lcc/Main.hs
@@ -3,25 +3,48 @@ import Parser
 import Pretty
 import Data.Maybe
 
---nameless variable for use with normal and term types
---you need to ensure that lookup correctly chooses the most recent binding of a variable from the list
---app does not (and should not) interact with deps, the only purpose of dep is to represent the type of a lam
-
 {-
-getType :: E -> Maybe E
---types of literal terms are the types of those terms
-getType (Lit (B _)) = Just $ LitT Bool
-getType (Lit (I _)) = Just $ LitT Int
---type of literal types is kind * (but so is the type of functions from type to type, which means that dep needs to reflect this)
-getType (LitT _) = Just Star
---type of star is box (but so is the type of functions from star to star, which means that dep needs to reflect this)
-getType Star = Just Box
---type of box is nothing
-getType Box = Nothing --there is no type of a type of a kind
+Current plans:
+
+Either find a way to map variable names or (for now) simply make var a (Int, String) and set its instance for
+Eq to equating ONLY the indecies. Then "show" can just use the string associated with every variable.
+Since lams don't need indecies in de brujin notation, they can just store a string which will correspond to the same string of the
+variables that it binds.
+
+With that out of the way, change the parser to accomodate the new definition of var and instantiate vars with a default,
+meaningless index value.
+
+Once the whole expression has been parsed, you can feed it to another function which recurses down the structure with a context:
+[(Name, Int)] and stores the names of lams it sees and increments every int in the tuples every time it goes down another lam.
+
+This shouldn't be too hard except for the fact that there is no way to deal with free variables. Thinking about it, I don't know
+if I necessarily need to deal with free variables since they effectively correspond to undefined variables which will end up
+failing at the type checking stage anyway. If I did want to deal with free variables, I could simply set the index of every
+free variable to the first non-bound index at that level although this would have the effect of making all free variables be the
+same free variable, regardless of whether or not they have the same variable name.
+
+This doesn't matter either though since there is no way to do useful computation with "truely" free variables. (I.e. variables
+which are free in the initial, top-level expression). So basically the system has the visual ids (the names) and the comptational
+ids (the indecies). The visual ids for free variables work fine. The computational ids do not work fine but free variables can
+never be used in computation anyway since they don't reference anything else in the expression and hence can never be used in
+subtitution.
+
+Once the indecies have been added to the variables, I can use the shift, sub and beta functions in the brujin example I wrote
+to do beta reduction pretty easily. I think this is only defined for Lam (not Pi) but I will have to see. I also don't think
+that the types in (Lam Name E E) (i.e. the first "E") are used in Beta reduction either. Certainly, since they are expressions
+you could beta reduce them but I don't think it is necessary but we will also see about that too.
+
+One more thing to mention is that since Nic is fairly convinced that the encoding to get show to correctly display different
+types of functions (e.g. a -> b rather than Pi all the time) is very hard or impossible, I will simply have an indicator in the Pi
+consturct itself which will be assigned during parsing. Simply put, if the expression is parsed as an arrow, then it will be
+shown as an arrow. If it's parsed as a Pi, it will be shown as a Pi.
+
+This brings me to my last thought which is that I need a consistent and fully funcitoning way to handle the underscore (empty
+variable name) in Lams and Pis. Simply put, the underscore means that there are will be no references to the input of that function
+in the function's output. Not only does this mean that you can write function types with the -> but it also means that during
+beta reduction, you need only produce the output exactly as is, without performing any substitution or anything.
+
+Finally, I really think I should test my brujin implementation on some other expressions before I assume that it is
+correct because I had to make 2 odd changes to a mathematical definition of it in a paper. But I can't seem to find any
+at the moment. 
 -}
-
---termToTerm :: E --the TYPE of a function from terms to terms
---termToTerm = Dep "_" a b where (type a, type b) = (Star, Star)
-
---typeToType :: E --the TYPE of a function from types to types
---typeToType = Dep "_" a b where (type a, type b) = (Box, Box)
